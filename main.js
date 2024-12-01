f8ac206ea5dec325f53fb30248ed13b399d2c2f8
Troy Kubanka <118857635+Troy-Kubanka@users.noreply.github.com>
2024-11-30 23:22:52 -0500

    Update main.js

--- main.js
+++ main.js
@@ -30,10 +30,37 @@ var typed = new Typed(".text", {
             && params.email.substring && !specialCharactersRegex.test(lettersAfterAtSign)   // tests if there is characters after @, and none of those characters after @ are special
             && params.email.charAt(params.email.indexOf("@")+1)!='.'    // confirms there is no . right after @ or .. after @
           ) { 
+            /*
               emailjs.send("service_q6vf7ud", "template_f5hlnzw", params).then(
                                                                           alert("Message successfully sent. Thank You " + 
                                                                           params.name)); 
                                                                                       // notify submission successful
+  */
+//
+            emailjs.send("service_q6vf7ud", "template_f5hlnzw", params)
+  .then(() => {
+    Swal.fire({
+      icon: 'success',
+      title: 'Message Sent!',
+      text: `Thank you, ${params.name}!`,
+      confirmButtonText: 'OK'
+    });
+  })
+  .catch((error) => {
+    Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: 'Something went wrong. Please try again later.',
+    });
+    console.error("EmailJS error:", error);
+  });
+
+
+
+
+            //
+
+            
                // to remove info and reload page
                document.getElementById("myForm").reset();    //clear contact Info    
                location.reload(true);           // reload page                                            
